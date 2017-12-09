e8d50a5578d2eb765a3e4d6e4bc5d5c3567b8cfb
KL-Lru <kilattoeruru@outlook.jp>
2017-12-10 00:31:20 +0900

    new commit

---
+++ Chapter1/00.py
@@ -0,0 +1,6 @@
+#
+#文字列"stressed"の文字を逆に並べた文字列を得よ
+#
+s="stressed"
+ans=s[::-1]
+print(ans)
---
+++ Chapter1/03.py
@@ -0,0 +1,8 @@
+#
+#文を単語に分解し，各単語の文字数を先頭から出現順に並べたリストを作成せよ．
+#
+s="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+ans=[]
+for i in s.replace(","," ").replace("."," ").split():
+  ans.append(len(i))
+print(ans)
