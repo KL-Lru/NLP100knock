4664dbba8bf56639808bd22823b0cf8ce67fbe92
KL-Lru <kilattoeruru@outlook.jp>
2017-12-11 03:52:29 +0900

    new

--- Chapter1/p05.py
+++ Chapter1/p05.py
@@ -2,22 +2,23 @@
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
 # この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
 #
-
 def n_gram(n,mode,s):
   ret=[]
   ret_unic=[]
   if mode=='w':
     l=s.split()
-    for i in range(0,len(l)-1,1):
-      ret.append(l[i]+" "+l[i+1])
+    tp=[]
+    for i in range(0,len(l)-n+1,1):
+      ret.append(" ".join(l[i:i+n:1]))
   elif mode=='c':
     s1=s.replace(" ","")
-    for i in range(0,len(s1)-1,1):
-      ret.append(s1[i]+s1[i+1])
+    for i in range(0,len(s1)-n+1,1):
+      ret.append(s1[i:i+n:1])
   for i in ret:
     if i not in ret_unic:
       ret_unic.append(i)
   return ret_unic
+#end def
 
 if __name__=="__main__":
   ans=n_gram(n=2,mode='w',s="I am an NLPer")
--- Chapter1/p06.py
+++ Chapter1/p06.py
@@ -5,40 +5,14 @@
 import p05 
 import pprint as p
 
-def uni(l1,l2):
-  ret=l1+l2
-  ret_unic=[]
-  for i in ret:
-    if i not in ret_unic:
-      ret_unic.append(i)
-  return ret_unic
-
-def pro(l1,l2):
-  ret=[]
-  for i in l1:
-    if i in l2:
-      ret.append(i)
-  return ret
-
-def dif(l1,l2):
-  ret=[]
-  for i in l1:
-    if i not in l2:
-      ret.append(i)
-  for i in l2:
-    if i not in l1:
-      ret.append(i)
-  return ret
-
-if __name__=='__main__':
-  s1="paraparaparadise"
-  s2="paragraph"
-  x=p05.n_gram(n=2,mode='c',s=s1)
-  y=p05.n_gram(n=2,mode='c',s=s2)
-  ans=[]
-  ans.append(["uni",uni(x,y)])
-  ans.append(["pro",pro(x,y)])
-  ans.append(["dif",dif(x,y)])
-  ans.append(["se in x?","se" in x])
-  ans.append(["se in y?","se" in y])
-  p.pprint(ans)
+s1="paraparaparadise"
+s2="paragraph"
+x=set(p05.n_gram(n=2,mode='c',s=s1))
+y=set(p05.n_gram(n=2,mode='c',s=s2))
+ans=[]
+ans.append(["uni",x.union(y)])
+ans.append(["pro",x.intersection(y)])
+ans.append(["dif",x.symmetric_difference(y)])
+ans.append(["se in x?","se" in x])
+ans.append(["se in y?","se" in y])
+p.pprint(ans)
