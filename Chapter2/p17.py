4f5aaab0a6661e28713212474fc77e1d31d0d94e
KL-Lru <kilattoeruru@outlook.jp>
2017-12-20 05:35:03 +0900

    new

---
+++ Chapter2/p17.py
@@ -0,0 +1,12 @@
+#
+# awk -F'\t' '{print $1}' hightemp.txt | sort | uniq | wc -l
+#
+f=open("hightemp.txt","r")
+li={}
+for i in f.readlines():
+  ti=i.split("\t")
+  li[ti[0]]=ti[1:-1]
+ans=[]
+for i in li:
+  ans.append(i)
+print(len(ans))
---
+++ Chapter2/p18.py
@@ -0,0 +1,19 @@
+#
+# sort -k3 hightemp.txt
+#
+f=open("hightemp.txt","r")
+ans=[]
+for i in f.readlines():
+  ans.append(i.split("\t"))
+# lambda式使うとこれだけで出来るらしい、よくわからんので割愛して無難なバブルソート
+# ans.sort(key=lambda x:x[2]) 
+for i in range(len(ans)):
+  for j in range(len(ans)-1,i,-1):
+    if ans[j][2] < ans[j-1][2]:
+      tmp=ans[j]
+      ans[j]=ans[j-1]
+      ans[j-1]=tmp
+for i in range(len(ans)):
+  ans[i]="\t".join(ans[i])
+ans="".join(ans)
+print(ans)
