d09dd0ad454aee2ccd1f06aeb9768eeddda241f9
KL-Lru <kilattoeruru@outlook.jp>
2018-05-26 12:03:33 +0900

    ミス修正

--- Chapter5/p47.py
+++ Chapter5/p47.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
         for si in chunk.srcs:
           if sentence[si].morphs[-1].surface == "を" and sentence[si].morphs[-2].pos1 == "サ変接続":
             predicate = sentence[si].morphs2str() + chunk.morphs[0].base
-        src = " ".join([x.morphs2str() for x in sorted([sentence[x] 
-                                               for x in chunk.srcs 
-                                               if sentence[x].morphs[-1].pos == "助詞" and x != si],
-                                       key = lambda x : x.morphs[-1].surface)])
-        con = " ".join(sorted([sentence[x].morphs[-1].surface 
-                               for x in chunk.srcs 
-                               if sentence[x].morphs[-1].pos == "助詞" and x != si]))
-        if predicate != "" and src != '':
-          print(predicate \
-                + "\t" + con \
-                + "\t" + src)
+            src = " ".join([x.morphs2str() for x in sorted([sentence[x] 
+                                                  for x in chunk.srcs 
+                                                  if sentence[x].morphs[-1].pos == "助詞" and x != si],
+                                          key = lambda x : x.morphs[-1].surface)])
+            con = " ".join(sorted([sentence[x].morphs[-1].surface 
+                                  for x in chunk.srcs 
+                                  if sentence[x].morphs[-1].pos == "助詞" and x != si]))
+            if predicate != "" and src != '':
+              print(predicate \
+                    + "\t" + con \
+                    + "\t" + src)
