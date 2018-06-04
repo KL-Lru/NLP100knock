06e1ad4d82e2631bb4bbfc6857c2fa77d6187950
KL-Lru <kilattoeruru@outlook.jp>
2018-06-04 16:00:17 +0900

    refactor Chapter5

--- Chapter5/p49.py
+++ Chapter5/p49.py
@@ -1,5 +1,6 @@
-# 二回もgitにファイルを消されてキレているためコメントは知らない
+#!/usr/bin/env python3
 # 
+# 名詞間の係り受けパスの抽出
 #
 
 from p40 import Morph
@@ -8,53 +9,58 @@ from p43 import contain_noun
 from copy import deepcopy
 
 if __name__ == "__main__":
-  for sentence in getChunks()[5:6]:
-    nouns=[]
+  # for sentence in getChunks()[5:6]: # 見易い出力例
+  for sentence in getChunks():
+    # 名詞句集め
+    nouns = []
     for nc in range(len(sentence)):
       if contain_noun(sentence[nc]):
         nouns.append(nc)
-    path=[]
+    
+    # パス計算
+    path = []
     for i in nouns:
-      l=[i]
-      d=sentence[i]
-      while(d.dst!=-1):
+      l = [i]
+      d = sentence[i]
+      while(d.dst != -1):
         l.append(d.dst)
-        d=sentence[d.dst]
+        d = sentence[d.dst]
       path.append(l)
 
     for ni in range(len(nouns)):
-      for nj in range(ni+1,len(nouns)):
+      for nj in range(ni + 1,len(nouns)):
+        # パスの交点はパスの積(集合)の最小値
         cross=min(set(path[ni]).intersection(set(path[nj])))
 
-        ans=""
+        answer = ""
         if cross == nouns[nj]:
-          d=deepcopy(sentence[ni])
+          d = deepcopy(sentence[nouns[ni]]) # shallowコピーだと元の内容まで変えてしまうので
           while(d.morphs and d.morphs[0].pos == "名詞"):
             del d.morphs[0]
-          d.morphs.insert(0,Morph("X","","",""))
+          d.morphs.insert(0, Morph("X", "", "", ""))
           while(d.dst != sentence[cross].dst):
-            ans+=d.morphs2str()+"->"
-            d=sentence[d.dst]
-          ans+="Y"
+            answer += d.morphs2str()+" -> "
+            d = sentence[d.dst]
+          answer += "Y"
 
         else:
-          d=deepcopy(sentence[ni])
+          d = deepcopy(sentence[nouns[ni]])
           while(d.morphs and d.morphs[0].pos == "名詞"):
             del d.morphs[0]
-          d.morphs.insert(0,Morph("X","","",""))
+          d.morphs.insert(0, Morph("X", "", "", ""))
           while(d.dst != cross):
-            ans+=d.morphs2str()+"->"
-            d=sentence[d.dst]
-          ans+=d.morphs2str()+"|"
-          d=deepcopy(sentence[nj])
+            answer += d.morphs2str() + " -> "
+            d = sentence[d.dst]
+          answer += d.morphs2str() + " | "
+          d = deepcopy(sentence[nouns[nj]])
           while(d.morphs and d.morphs[0].pos == "名詞"):
             del d.morphs[0]
-          d.morphs.insert(0,Morph("Y","","",""))
+          d.morphs.insert(0, Morph("Y", "", "", ""))
           while(d.dst != cross):
-            ans+=d.morphs2str()+"->"
-            d=sentence[d.dst]
-          ans+=d.morphs2str()+"|"
-          d=sentence[d.dst]
-          ans+=d.morphs2str()+"|"
-        print(ans)         
+            answer += d.morphs2str() + " -> "
+            d = sentence[d.dst]
+          answer += d.morphs2str() + " | "
+          d = sentence[d.dst]
+          answer += d.morphs2str()
+        print(answer)         
       
