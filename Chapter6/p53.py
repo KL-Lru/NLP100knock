bab73e757680b524e869d064413301067bf54619
KL-Lru <kilattoeruru@outlook.jp>
2018-12-05 03:26:09 +0900

    solve p56

--- Chapter6/p53.py
+++ Chapter6/p53.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bs
+#java -Xmx2g -cp "/usr/local/lib/stanford-corenlp-full-2018-02-27/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators "tokenize,ssplit,pos,lemma,ner,regexner,truecase,parse,depparse,dcoref,relation,natlog,quote,sentiment" -file nlp.txt -outputFormat xml
 
 if __name__ == '__main__':
   f = open('nlp.txt.xml')
